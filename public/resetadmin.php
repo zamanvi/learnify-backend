262e994df1c635750a4602c9162b5d4895686a30
zamanvi <norozzaman996@gmail.com>
2026-06-22 14:15:31 +0600

    Temp: reset admin passwords

---
+++ public/resetadmin.php
@@ -0,0 +1,15 @@
+﻿<?php
+require __DIR__ . '/../vendor/autoload.php';
+$app = require __DIR__ . '/../bootstrap/app.php';
+$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+$newPassword = 'Admin@1234';
+$hashed = Hash::make($newPassword);
+
+DB::table('users')->where('user_type', '1')->update(['password' => $hashed]);
+
+$admins = DB::table('users')->where('user_type', '1')->get(['id','name','email']);
+echo json_encode(['new_password' => $newPassword, 'admins_updated' => $admins]);
