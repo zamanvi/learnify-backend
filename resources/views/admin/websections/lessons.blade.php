0a77403bff33c7a2a8229ac5207ce2464a4511bd
zamanvi <norozzaman996@gmail.com>
2026-09-08 11:29:52 +0600

    Revert "feat: re-add SEO fields backfill + model/view changes (safer version)"
    This reverts commit b82d35622d6f72e894679106f855adadeeeb733d.

--- resources/views/admin/websections/lessons.blade.php
+++ resources/views/admin/websections/lessons.blade.php
@@ -77,18 +77,6 @@
                                 <textarea name="content" class="form-control" id="content" rows="6"></textarea>
                             </div>
                             <div class="form-group">
-                                <label for="short_details">Short Details (SEO meta description)</label>
-                                <input type="text" name="short_details" class="form-control" id="short_details">
-                            </div>
-                            <div class="form-group">
-                                <label for="link">Video Link (YouTube, optional)</label>
-                                <input type="text" name="link" class="form-control" id="link">
-                            </div>
-                            <div class="form-group">
-                                <label for="keyword">Keywords (SEO, optional)</label>
-                                <input type="text" name="keyword" class="form-control" id="keyword">
-                            </div>
-                            <div class="form-group">
                                 <label for="order">Order</label>
                                 <input type="number" name="order" class="form-control" id="order" value="0">
                             </div>
